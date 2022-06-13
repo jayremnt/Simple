25b5bc70ada7d783ada30a1220278bd3efb6c425
Jayremnt <jayremnt@gmail.com>
2022-06-13 14:53:11 +0700

    added partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     locale = params[:locale].to_s.strip.to_sym
     I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
   end
+
+  def hello
+    render html: "Hello"
+  end
 end
