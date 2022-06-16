704fe958d5e4b21ba37e9897801246d3e8559863
Jayremnt <jayremnt@gmail.com>
2022-06-16 16:24:47 +0700

    fix can not show sign up successful papage, add translations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in user
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = t('app.welcome')
       return redirect_to @user
     end
     render :new
